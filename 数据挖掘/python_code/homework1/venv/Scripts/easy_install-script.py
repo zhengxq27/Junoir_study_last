1aec5e1c88dd3a5830cee8b3c05934de4d4308ab
zhengxq27 <1172009120@qq.com>
2019-03-15 19:40:28 +0800

    add

---
+++ 数据挖掘/python_code/homework1/venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\admin\Desktop\大三下\Junoir_study_last\数据挖掘\python_code\homework1\venv\Scripts\python.exe -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
+__requires__ = 'setuptools==39.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.1.0', 'console_scripts', 'easy_install')()
+    )
